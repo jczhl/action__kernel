a306730254111bead6660321094e382ac2e1b48d
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2022-05-24 11:53:30 +0800

    Update gcc4.9.sh

--- gcc4.9.sh
+++ gcc4.9.sh
@@ -3,7 +3,7 @@ export ARCH=arm64
 export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-linux-androideabi-
 echo "===================Setup Environment==================="
-cd $GITHUB_WORKSPACE/out
+cd $GITHUB_WORKSPACE/kernel
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 -b lineage-18.1 tool/aarch
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 -b lineage-18.1 tool/arm
 sh -c "$(curl -sSL https://github.com/akhilnarang/scripts/raw/master/setup/android_build_env.sh/)"
