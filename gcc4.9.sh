401b6d6819dffcbab3ebe21d770b280b8c7ac8f2
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2022-05-24 11:04:10 +0800

    Update gcc4.9.sh

--- gcc4.9.sh
+++ gcc4.9.sh
@@ -10,5 +10,5 @@ sh -c "$(curl -sSL https://github.com/akhilnarang/scripts/raw/master/setup/andro
 echo "=========================Clean========================="
 make mrproper && git reset --hard HEAD
 echo "=========================make========================="
-make O=out m1721_defconfig
+make O=out m3note_defconfig
 make O=out
