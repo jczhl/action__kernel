aed36ed491e4a77f546efdbc0497d7ea39a99b95
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2023-02-27 10:53:36 +0800

    Update clang.sh

--- clang.sh
+++ clang.sh
@@ -1,17 +1,19 @@
 echo "===================Setup Export========================="
-export ANYKERNEL_PATH=~/Anykernel3
-export CLANG_PATH=~/prelude-clang
+export KERNEL_PATH=$PWD
+export CLANG_PATH=~/proton-clang
 export PATH=${CLANG_PATH}/bin:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export SUBARCH=arm64
-echo "===================Setup Environment==================="
-git clone --depth=1 https://gitlab.com/jjpprrrr/prelude-clang.git $CLANG_PATH
-git clone https://github.com/osm0sis/AnyKernel3 $ANYKERNEL_PATH
-sh -c "$(curl -sSL https://github.com/akhilnarang/scripts/raw/master/setup/android_build_env.sh/)"
-echo "=========================Clean========================="
-make mrproper && git reset --hard HEAD
-echo "=========================Build========================="
-make O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld m1721_defconfig
-make O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld 2>&1 | tee out/kernel.log
 
+echo "===================Setup Environment==================="
+git clone --depth=1 https://github.com/kdrag0n/proton-clang $CLANG_PATH
+sh -c "$(curl -sSL https://github.com/akhilnarang/scripts/raw/master/setup/android_build_env.sh/)"
+
+echo "=========================Clean========================="
+rm -rf $KERNEL_PATH/out/ *.zip
+make mrproper && git reset --hard HEAD
+
+echo "=========================Make=========================="
+make O=out CC="ccache clang" CXX="ccache clang++" ARCH=arm64 CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld vendor/kona-perf_defconfig
+make O=out CC="ccache clang" CXX="ccache clang++" ARCH=arm64 CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld 2>&1 | tee kernel.log
